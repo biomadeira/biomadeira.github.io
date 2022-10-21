e6ef06e39de8c311c738fa1f245d5e8d416706bc
Fábio Madeira <fabiomadeira@me.com>
2022-10-21 15:48:13 +0100

    Minor change but big improvement on compatibility with plugins that use "paginate"

--- _plugins/jekyll-tagsgenerator.rb
+++ _plugins/jekyll-tagsgenerator.rb
@@ -17,7 +17,7 @@ module Jekyll
     end
 
     def paginate(site, type, posts)
-      pages = Jekyll::Paginate::Pager.calculate_pages(posts[1], site.config['paginate'].to_i)
+      pages = Jekyll::Paginate::Pager.calculate_pages(posts[1], site.config['per_page'].to_i)
       (1..pages).each do |num_page|
         pager = Jekyll::Paginate::Pager.new(site, num_page, posts[1], pages)
         path = "/tag/" + posts[0].slugify
